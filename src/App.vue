7343f50289596c883c8435387c9d84b7c8af7227
Frank Tan <909628250@qq.com>
2019-12-04 18:35:18 +0800

    project init

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="app">
+        <nav-header></nav-header>
+        <router-view />
+    </div>
+</template>
+
+<script>
+    import NavHeader from './components/NavHeader';
+    export default {
+        name: 'App',
+        components: {
+            NavHeader
+        }
+    };
+</script>
+
+<style lang="scss" scoped>
+</style>
