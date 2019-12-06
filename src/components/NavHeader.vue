d34fd6834aa2a177f7c08b50838526dbc9ba48f3
Frank Tan <909628250@qq.com>
2019-12-06 18:35:08 +0800

    Modal importation

--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -1,67 +1,80 @@
 <template>
-    <el-header class="header">
-        <el-container class="header-nav">
-            <a href="/" class="header-nav-logo">
+    <el-container class="header">
+        <el-header class="header-nav">
+            <a href="/" class="logo">
                 <img class="navbar-brand-logo" src="../assets/img/logo.png">
             </a>
 
-            <el-container>
-                <span class="navbar-link">nickname</span>
-                <a href="javascript:;" class="navbar-link">Sign in</a>
-                <a href="javascript:;" class="navbar-link">Sign out</a>
-                <div class="navbar-cart-container">
-                    <span class="navbar-cart-count">1</span>
-                    <a class="navbar-link navbar-cart-link" href="/#/cart">
+            <div class="account">
+                <span class="account-link">nickname</span>
+                <a href="javascript:;" class="account-link" @click="dialogVisible = true">Sign in</a>
+                <a href="javascript:;" class="account-link">Sign out</a>
+                <div class="cart-container">
+                    <span class="cart-count">1</span>
+                    <a class="cart-link" href="/#/cart">
                         <i class="el-icon-shopping-cart-2"></i>
                     </a>
                 </div>
-            </el-container>
-        </el-container>
-        
-
-        <el-container class="header-nav-account" direction="vertical">
-            
-            <div class="md-modal modal-msg md-modal-transition" style="display:none;">
-            <div class="md-modal-inner">
-                <div class="md-top">
-                    <div class="md-title">Login in</div>
-                    <button class="md-close">Close</button>
-                </div>
-                <div class="md-content">
-                    <div class="confirm-tips">
-                        <div class="error-wrap">
-                            <span class="error error-show">用户名或者密码错误</span>
-                        </div>
-                        <ul>
-                            <li class="regi_form_input">
-                                <i class="icon IconPeople"></i>
-                                <input type="text" tabindex="1" name="loginname"
-                                    class="regi_login_input regi_login_input_left" placeholder="User Name"
-                                    data-type="loginname">
-                            </li>
-                            <li class="regi_form_input noMargin">
-                                <i class="icon IconPwd"></i>
-                                <input type="password" tabindex="2" name="password"
-                                    class="regi_login_input regi_login_input_left login-input-no input_text"
-                                    placeholder="Password">
-                            </li>
-                        </ul>
-                    </div>
-                    <div class="login-wrap">
-                        <a href="javascript:;" class="btn-login">登 录</a>
-                    </div>
-                </div>
             </div>
-        </div>
-        <div class="md-overlay"></div>
-            
-        </el-container>   
-    </el-header>
+        </el-header>
+        
+        <el-dialog
+            :visible.sync="dialogVisible"
+            width="30%"
+            top="0"
+            center
+            title="Sign in">
+            <span slot="footer" class="dialog-footer">
+                <el-button @click="dialogVisible = false">取 消</el-button>
+                <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+            </span>
+        </el-dialog>
+
+        <!-- <el-dialog class="sign-container">
+            <div class="md-modal modal-msg md-modal-transition">
+                <div class="md-modal-inner">
+                    <div class="md-top">
+                        <div class="md-title">Login in</div>
+                        <button class="md-close">Close</button>
+                    </div>
+                    <div class="md-content">
+                        <div class="confirm-tips">
+                            <div class="error-wrap">
+                                <span class="error error-show">用户名或者密码错误</span>
+                            </div>
+                            <ul>
+                                <li class="regi_form_input">
+                                    <i class="icon IconPeople"></i>
+                                    <input type="text" tabindex="1" name="loginname"
+                                        class="regi_login_input regi_login_input_left" placeholder="User Name"
+                                        data-type="loginname">
+                                </li>
+                                <li class="regi_form_input noMargin">
+                                    <i class="icon IconPwd"></i>
+                                    <input type="password" tabindex="2" name="password"
+                                        class="regi_login_input regi_login_input_left login-input-no input_text"
+                                        placeholder="Password">
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="login-wrap">
+                            <a href="javascript:;" class="btn-login">登 录</a>
+                        </div>
+                    </div>
+                </div>
+            </div>    
+        </el-dialog>    -->
+    </el-container>
 </template>
 
 <script>
     export default {
         name: 'NavHeader',
+        data() {
+            return {
+                dialogVisible: false
+            }
+        }
     }
 </script>
 
@@ -75,14 +88,41 @@
         flex-direction: column;
         
         &-nav {
-            @include flex-between;
             width: 100%;
             padding: 5px 30px 10px;
             max-width: 1280px;
+            min-width: 375px;
             margin: 0 auto;
+            @include flex-between(); 
 
-            .el-container{
-                flex: 0;
+            .account {
+                @include flex-between();
+
+                &-link {
+                    padding-left: 15px;
+                }
+
+                .cart-container{
+                    padding-left: 15px;
+                    position: relative;
+                    
+                    .cart-link {
+                        font-size: $icon-font-size;
+                    }
+
+                    .cart-count{
+                        position: absolute;
+                        top: -6px;
+                        right: -10px;
+                        width: 20px;
+                        height: 20px;
+                        @include flex-center();
+                        background-color: #eb767d;
+                        color: #fff;
+                        font-weight: 700;
+                        border-radius: 50%;
+                    }
+                }
             }
         }
 
@@ -90,5 +130,10 @@
             color: #666;
             text-decoration: none;
         }
+    }
+
+    .el-dialog__wrapper {
+        @include flex-center();
+
     }
 </style>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -3,8 +3,12 @@ import { Button } from 'element-ui'
 import { Icon } from 'element-ui'
 import { Header } from 'element-ui'
 import { Container } from 'element-ui'
+import { Row } from 'element-ui'
+import { Dialog } from 'element-ui'
 
 Vue.use(Button)
 Vue.use(Icon)
 Vue.use(Header)
 Vue.use(Container)
+Vue.use(Row)
+Vue.use(Dialog)
